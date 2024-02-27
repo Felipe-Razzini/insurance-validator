cd475013162570f710d32189dc33381f93094f4c
Felipe Razzini <feliperazzini95@gmail.com>
2024-02-27 17:49:36 +0100

    javascript for displaying and hidding form

--- app.js
+++ app.js
@@ -3,7 +3,27 @@ import { createApp } from 'vue'
 createApp({
   data() {
     return {
-      message: "If you see this message in your browser, that means Vue is successfully mounted! 🙌"
+      showClientForm: false
+    }
+  },
+  methods: {
+    displayForm() {
+      this.showClientForm = true
+      this.toggleFormVisibility(true);
+    },
+    hideForm() {
+      this.showClientForm = false
+      this.toggleFormVisibility(false);
+    },
+    toggleFormVisibility(isVisible) {
+      const form = document.getElementById('client-form');
+      if (isVisible) {
+          setTimeout(() => {
+              form.classList.add('show');
+          }, 0);
+      } else {
+          form.classList.remove('show');
+      }
     }
   }
 }).mount('#app')
